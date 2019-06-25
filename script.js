c48c1a6680b7fb1deae943447285e68ce1eaecf3
Glitch (hello-express) <none>
2019-06-25 17:51:01 +0100

    Some java

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-
 function blinker() {
 	$('.header__title-element').fadeOut(500);
 	$('.header__title-element').fadeIn(500);
 }
 setInterval(blinker(), 1000);
+
+const inputDate;
+const submitBtn = document.getElementById('main__submit-date');
+
+submit.submit('click',function(e){
+
+  e.preventDefault();
+  inputDate = document.getElementById('main__input-date');
+
+},false);
+
+console.log(inputDate).toString();
