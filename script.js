a99e90469216c9df7e08595b29b61fbcbd6b8d85
Glitch (hello-express) <none>
2019-06-26 10:30:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ submitBtn.addEventListener('click',function(e){
 
   e.preventDefault();
 
-	var inputDate = document.getElementById('main__input-date');
+	var inputDate = document.getElementById('main__input-date').value;
 	console.log(subtractDates(inputDate));
 
 },false);
@@ -20,7 +20,7 @@ function subtractDates(date){
 
 	var input = new Date(date);
 	var today = new Date();
-	console.log(today + "subtract" + input);
+	console.log(today + " subtract " + input);
   return today.getTime() - input.getTime();
 
 }
