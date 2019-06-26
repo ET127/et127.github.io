5608570173999b49f1f3d1064e872fab2a3bf7f1
Glitch (hello-express) <none>
2019-06-26 10:50:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ function subtractDates(date){
 	console.log(today + " subtract " + input);
 	var diff = input.getTime() - today.getTime();
 
-	var secs = Math.round(diff / 1000);
+	var secs = diff / 1000;
 	var minutes = secs/60;
 	var hours = minutes / 60;
 	var days = hours / 24;
 
-  return days + ":" + hours + ":" + minutes % 60 + ":" + secs % 60;
+  return Math.round(days) + ":" + Math.round(hours) + ":" +Math.round(minutes) % 60 + ":" + Math.round(secs) % 60;
 
 }
