ba0b8a08c9c10ec75661da61f0dc73d282305faf
Glitch (hello-express) <none>
2019-06-26 12:39:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function blinker() {
-	$('.header__title-element').fadeOut(500);
-	$('.header__title-element').fadeIn(500);
+  $('.header__title-element').fadeOut(500);
+  $('.header__title-element').fadeIn(500);
 }
 setInterval(blinker(), 1000);
 
@@ -8,40 +8,48 @@ const dateOutput = document.getElementById('main__date-output');
 const listOutput = document.getElementById('main__list-output');
 let list = [''];
 
-setInterval(function(){
+setInterval(function() {
 
-	var inputDate = document.getElementById('main__input-date').value;
-	console.log();
-	dateOutput.innerHTML = subtractDates(inputDate);
+  var inputDate = document.getElementById('main__input-date').value;
+  console.log();
+  dateOutput.innerHTML = subtractDates(inputDate);
 
-},1000);
+}, 1000);
 
-function subtractDates(date){
+function subtractDates(date) {
 
-	var input = new Date(date);
-	var today = new Date();
-	console.log(today + " subtract " + input);
-	var diff = input.getTime() - today.getTime();
+  var input = new Date(date);
+  var today = new Date();
+  console.log(today + " subtract " + input);
+  var diff = input.getTime() - today.getTime();
 
-	var secs = Math.floor(diff / 1000);
-	var min = Math.floor(secs/60);
-	var hours = Math.floor(min / 60);
-	var days = Math.floor(hours / 24);
+  if (diff != NaN) {
+
+    var secs = Math.floor(diff / 1000);
+    var min = Math.floor(secs / 60);
+    var hours = Math.floor(min / 60);
+    var days = Math.floor(hours / 24);
 
 
-  return days + " days, " + hours % 24 + " hours, " + min % 60  + " minutes and " + secs % 60 + " seconds left!";
+    return days + " days, " + hours % 24 + " hours, " + min % 60 + " minutes and " + secs % 60 + " seconds left!";
+
+  } else {
+
+    return "Input a date!";
+
+  }
 
 }
 
-function submit(){
+function submit() {
 
-	addToList(document.getElementById('main__input-list-ite').value);
+  addToList(document.getElementById('main__input-list-item').value);
 
 }
 
-function addToList(item){
+function addToList(item) {
 
-		list.push(item);
-		listOutput.innerHTML = list.toString() + "<br>";
+  list.push(item);
+  listOutput.innerHTML = list.toString() + "<br>";
 
 }
